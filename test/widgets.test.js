c1ffaf92030443544b0d5c0b081c9da99cfe4c6b
Jannis Kammann <jannis@kammann.cc>
2025-08-28 18:21:43 +0200

    removed widget test

--- test/widgets.test.js
+++ test/widgets.test.js
@@ -13,17 +13,6 @@ describe('vis-2-widgets-react-template', () => {
         await helper.screenshot(null, '02_widgets_opened');
     });
 
-    it('Check Demo widget', async function () {
-        this.timeout(60000);
-        const widgets = await helper.palette.getListOfWidgets(null, 'demo');
-        for (let w = 0; w < widgets.length; w++) {
-            const wid = await helper.palette.addWidget(null, widgets[w], true);
-            await helper.screenshot(null, `10_${widgets[w]}`);
-            await helper.view.deleteWidget(null, wid);
-        }
-        return Promise.resolve();
-    });
-
     after(async function () {
         this.timeout(5000);
         await helper.stopBrowser();
